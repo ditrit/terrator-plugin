87cde69484205c188258f9fff593241fef52d6e4
Vincent Moittié <vincent.moittie@cji.paris>
2022-08-12 11:44:08 +0200

    Rename template name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'my-plugin.js',
+    filename: 'terrator-plugin.js',
     libraryTarget: 'commonjs',
   },
   resolve: {

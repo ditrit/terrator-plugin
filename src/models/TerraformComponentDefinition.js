94b1c510937e13463406e20cf78afed2f8a8048e
Vincent Moittié <vincent.moittie@cji.paris>
2022-08-24 10:40:47 +0200

    Add specific definition class to manage Terraform component

---
+++ src/models/TerraformComponentDefinition.js
@@ -0,0 +1,32 @@
+import { ComponentDefinition } from 'leto-modelizer-plugin-core';
+
+/**
+ * Specific Terraform component definition.
+ * @extends {ComponentDefinition}
+ */
+class TerraformComponentDefinition extends ComponentDefinition {
+  /**
+   * Override ComponentDefinition construction with blockType and provider properties.
+   * @param {String} [props.blockType] - Type of Terraform block.
+   * @param {String} [props.provider] - Related provider of terraform block.
+   * @see ComponentDefinition
+   */
+  constructor(props = {
+    blockType: null,
+    provider: null,
+  }) {
+    super(props);
+    /**
+     * Type of Terraform block.
+     * @type {String}
+     */
+    this.blockType = props.blockType || null;
+    /**
+     * Related provider of the terraform block.
+     * @type {String}
+     */
+    this.provider = props.provider || null;
+  }
+}
+
+export default TerraformComponentDefinition;

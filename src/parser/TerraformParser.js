63841095b236f315f023793748771d635cc7b914
Vincent Moittié <moittie.vincent@gmail.com>
2023-02-02 14:59:17 +0100

    Initialize components before parsing

--- src/parser/TerraformParser.js
+++ src/parser/TerraformParser.js
@@ -26,6 +26,7 @@ class TerraformParser extends DefaultParser {
    * @param {FileInput[]} [inputs=[]] - Data you want to parse.
    */
   parse(inputs = []) {
+    this.pluginData.components = [];
     const listener = new TerraformListener(this.pluginData.definitions.components);
     inputs
       .filter(({ content }) => content !== null)

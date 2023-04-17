8f6471d910df6a1e164dca0f035cb5d67dfd5721
Justine Hell <hell.justine@gmail.com>
2023-04-17 17:23:07 +0200

    Update to plugin-core version 0.15.1

--- src/draw/TerraformDrawer.js
+++ src/draw/TerraformDrawer.js
@@ -4,8 +4,8 @@ import { DefaultDrawer } from 'leto-modelizer-plugin-core';
  * Class to draw Terraform components.
  */
 class TerraformDrawer extends DefaultDrawer {
-  constructor(pluginData, resources, events, rootId, options) {
-    super(pluginData, resources, events, rootId, {
+  constructor(pluginData, resources, rootId, options) {
+    super(pluginData, resources, rootId, {
       ...options,
       minHeight: 80,
       minWidth: 110,

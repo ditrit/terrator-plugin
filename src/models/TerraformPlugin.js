b1dd1a75d88c05ca6aca39cfa74c85d3fe177c6c
Vincent Moittié <moittie.vincent@gmail.com>
2023-06-06 10:27:55 +0200

    Fix eslint warnings

--- src/models/TerraformPlugin.js
+++ src/models/TerraformPlugin.js
@@ -15,7 +15,7 @@ import packageInfo from 'package.json';
 class TerraformPlugin extends DefaultPlugin {
   /**
    * Default constructor.
-   * @param {object} [props={}] - Object that contains all properties to set.
+   * @param {object} [props] - Object that contains all properties to set.
    * @param {object} [props.event] - Event manager.
    * @param {Function} [props.event.next] - Function to emit event.
    */

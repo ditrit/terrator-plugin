d4b66bd0410aeadefab14879009438b41ce9249a
Vincent Moittié <moittie.vincent@gmail.com>
2024-08-08 14:55:44 +0200

    Remove import eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -61,11 +61,5 @@ module.exports = {
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'jsdoc/no-undefined-types': 'off',
-    'no-restricted-imports': ['error', {
-      patterns: [{
-        group: ['\\.\\./*', '\\./*'],
-        message: 'Use \'src/\' for base of import. Don\'t use \'./\' or \'../\'.',
-      }],
-    }],
   },
 };
